8a0c89db6b1af70cbaac69bedbb8d13f7c5dec4a
Joeli Takala <joeli.takala@helsinki.fi>
2021-04-08 14:19:48 +0300

    Päivitetty versio widgetistä HEROn julkaisua varten

--- widget.js
+++ widget.js
@@ -2,11 +2,35 @@
 var HERO = HERO || {};
 
 HERO = {
+
+    linkTarget: "http://heraldica.narc.fi/termi.html",
+    imageLocation: "http://heraldica.narc.fi/img/hero/thumb/",
     widget: {
+        getTranslation (key) {
+            var getLang = lang;
+            if (lang !== "fi" && lang !== "sv") {
+                getLang = "en";
+            }
+            if (key=='linkText') {
+                return {
+                    'fi': 'Vastaava sivu Europeana Heraldicassa',
+                    'sv': 'Motsvarande sida i Europeana Heraldica',
+                    'en': 'Corresponding page in Europeana Heraldica'
+                }[getLang];
+            } else {
+                return "";
+            }
+        },
         render: function (id) {
-            var imgUrl = 'http://heraldica.narc.fi/img/hero/thumb/' + id + '.png';
+            var imgUrl = HERO.imageLocation + id + '.png';
             $('.concept-main').prepend(Handlebars.compile($('#hero-template').html())({'imgurl': imgUrl}));
             $('.property-label-pref').css({'height': '105px'});
+        },
+        createLink: function (id) {
+            var $linkElement = $('<p class="hero-link"><a href="" target="_blank"><span></span></a></p>');
+            $linkElement.find('a').attr("href", HERO.linkTarget + "?id=" + id + "&lang=" + lang + "&t=" + content_lang);
+            $linkElement.find('span').text(HERO.widget.getTranslation('linkText'));
+            $(".concept-main").append($linkElement);
         }
     }
 };
@@ -21,6 +45,7 @@ $(function() {
         // reading the id from the uri
         var id = data.uri.substr(data.uri.lastIndexOf('/p') + 2); 
         HERO.widget.render(id);
+        HERO.widget.createLink(id);
     };
 
 });
